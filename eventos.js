8cb4f694c059cc7689499b258c99b3a096c40750
faqsalas <facundosalas10@gmail.com>
2023-08-09 18:34:20 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -3,3 +3,9 @@ const div = document.querySelector('.elDiv')
 div.addEventListener('click', () => {
     alert("Hola! Soy el div")
 })
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(event) {
+    event.stopPropagation();
+})
